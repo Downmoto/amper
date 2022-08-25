9fd5584398869a3c9f4cd834f6778f19e91b1ac4
Arad Fadaei <aradfadaie@gmail.com>
2022-08-25 17:33:44 -0400

    init

---
+++ webpack.main.config.js
@@ -0,0 +1,16 @@
+const port = process.env.PORT = 9001
+
+module.exports = {
+  /**
+   * This is the main entry point for your application, it's the first file
+   * that runs in the main process.
+   */
+  entry: './src/main/index.ts',
+  // Put your normal webpack config below here
+  module: {
+    rules: require('./webpack.rules'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
